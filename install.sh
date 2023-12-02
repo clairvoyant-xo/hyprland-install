f6aa5f53eef5dc3707bd9dad1c888a326d32c91d
Martin <73581070+clairvoyant-xo@users.noreply.github.com>
2023-12-02 05:57:19 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+git clone https://aur.archlinux.org/paru-bin.git
+cd paru-bin
+makepkg -si
+paru -S --needed hyprland waybar swaybg swayidle swaylock gammastep mako polkit-gnome xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xorg-xwayland qt5-wayland qt6-wayland noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome foot wofi firefox pcmanfm-gtk3 gvfs udisks2 file-roller featherpad swayimg mpv qt5ct nwg-look-bin wlogout
+mkdir -p ~/.config
+cd ..
+cp -r ./dotfiles/* ~/.config
+mv ~/.config/bash_profile ~/.bash_profile
